c64638a7c00083fe464f49d0653ab1da5ecc56e7
souzap371 <souzap371@gmail.com>
2022-11-18 08:59:41 -0300

    Funções

--- funcao/parametros_opcionais.dart
+++ funcao/parametros_opcionais.dart
@@ -9,10 +9,10 @@ main() {
   int n2 = numeroAleatorio();
   print(n2);
 
+//Retorno de parametros opcionais 2º exemplo, pode retornar valor informado na chamada, ou valores padrão.
   imprimirData(10, 12, 2020);
   imprimirData(10, 12);
   imprimirData(10);
-  imprimirData();
 }
 
 //Parametro opcional posicionado = separados por colchetes, aplicar valor padrão ao paramtro opcional
@@ -20,6 +20,7 @@ int numeroAleatorio([int maximo = 11]) {
   return Random().nextInt(maximo);
 }
 
-imprimirData([int dia = 1, int mes = 1, int ano = 1970]) {
+//Parametro opcional = mais um exemplo de parametros opcionais, o dia se torna obrigatorio, nao podendo retornar sem apontar o parametro DIA
+imprimirData(int dia, [int mes = 1, int ano = 1970]) {
   print('$dia/$mes/$ano');
 }
